8e12e30b4d9bff4c81331f065f491a21a70316b2
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-05-01 16:28:01 +0200

    GET functionality for our CRUD REST endpoints

---
+++ src/main/java/com/manal/conferencedemospringboot/controllers/SessionsController.java
@@ -0,0 +1,50 @@
+package com.manal.conferencedemospringboot.controllers;
+
+import com.manal.conferencedemospringboot.models.Session;
+import com.manal.conferencedemospringboot.repositories.SessionRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+//added so that Spring MVC knows that this is a controller
+//means that this will respond to payloads, incoming and outgoing as JSON REST endpoints
+@RestController
+//tells the router what the mapping URL will look like
+@RequestMapping("/api/v1/sessions")
+public class SessionsController {
+  //injects the sessionRepository that we created
+  //Spring will autowire this when our SessionController is built.
+  //Spring will create an instance of the SessionRepository and put it onto our class
+  @Autowired
+  private SessionRepository sessionRepository;
+
+  @GetMapping
+  public List<Session> list() {
+    return sessionRepository.findAll();
+  }
+
+  @GetMapping
+  @RequestMapping("{id}")
+  public Session get(@PathVariable Long id) {
+    return sessionRepository.getById(id);
+  }
+
+//  @PostMapping
+//  //Typically, when you create or post something you get a 201 back, but the
+//  //Spring controllers will by default return 200 when the method executes and finishes.
+//  //This annotation overrides that, we can specify the exact response we want
+//  @ResponseStatus(HttpStatus.CREATED)
+//  public Session create(@RequestBody final Session session) {
+//    return sessionRepository.saveAndFlush(session);
+//  }
+
+
+
+}
