6a02fad472db2b9f8cd5c7941036529bd76121af
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-05-01 17:11:02 +0200

    CREATE, UPDATE, DELETE functionality for our CRUD REST endpoints

--- src/main/java/com/manal/conferencedemospringboot/controllers/SessionsController.java
+++ src/main/java/com/manal/conferencedemospringboot/controllers/SessionsController.java
@@ -3,11 +3,14 @@ package com.manal.conferencedemospringboot.controllers;
 import com.manal.conferencedemospringboot.models.Session;
 import com.manal.conferencedemospringboot.repositories.SessionRepository;
 import java.util.List;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -36,15 +39,39 @@ public class SessionsController {
     return sessionRepository.getById(id);
   }
 
-//  @PostMapping
-//  //Typically, when you create or post something you get a 201 back, but the
-//  //Spring controllers will by default return 200 when the method executes and finishes.
-//  //This annotation overrides that, we can specify the exact response we want
-//  @ResponseStatus(HttpStatus.CREATED)
-//  public Session create(@RequestBody final Session session) {
-//    return sessionRepository.saveAndFlush(session);
-//  }
+  @PostMapping
+  //Typically, when you create or post something you get a 201 back, but the
+  //Spring controllers will by default return 200 when the method executes and finishes.
+  //This annotation overrides that, we can specify the exact response we want
+  @ResponseStatus(HttpStatus.CREATED)
+  public Session create(@RequestBody final Session session) {
+    return sessionRepository.saveAndFlush(session);
+  }
 
+  @DeleteMapping
+  @RequestMapping(value = "{id}")
+  public void delete(@PathVariable Long id) {
+    //also need to check cascades.
+    //this only deletes session records without any children records
+    //at this current implementation
+    sessionRepository.deleteById(id);
+  }
+
+  @PutMapping
+  @RequestMapping(value = "{id}")
+  public Session update(@PathVariable Long id, @RequestBody Session session) {
+    //because this is a PUT, we expect all attributes to be passed in.
+    //a Patch would only update passed values
+    //TODO: add validation that all attributes are passed in, otherwise return a 400 bad payload
+    //finding the session we're going to update
+    Session existingSession = sessionRepository.getById(id);
+    //takes the existing session and copies the incoming session data onto it, the third
+    //param allows us to ignore properties on the entities or Java
+    //object that we do not want to copy
+    BeanUtils.copyProperties(session, existingSession, "session_id");
+    return sessionRepository.saveAndFlush(existingSession);
+
+  }
 
 
 }
--- src/main/java/com/manal/conferencedemospringboot/controllers/SpeakersController.java
+++ src/main/java/com/manal/conferencedemospringboot/controllers/SpeakersController.java
@@ -3,10 +3,17 @@ package com.manal.conferencedemospringboot.controllers;
 import com.manal.conferencedemospringboot.models.Speaker;
 import com.manal.conferencedemospringboot.repositories.SpeakerRepository;
 import java.util.List;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,6 +32,26 @@ public class SpeakersController {
   @RequestMapping("{id}")
   public Speaker get(@PathVariable Long id) {
     return speakerRepository.getById(id);
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Speaker create(@RequestBody Speaker speaker) {
+    return speakerRepository.saveAndFlush(speaker);
+  }
+
+  @PutMapping
+  @RequestMapping("{id}")
+  public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
+    Speaker existingSpeaker = speakerRepository.getById(id);
+    BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+    return speakerRepository.saveAndFlush(existingSpeaker);
+  }
+
+  @DeleteMapping
+  @RequestMapping("{id}")
+  public void delete(@PathVariable Long id) {
+    speakerRepository.deleteById(id);
   }
 
 }
