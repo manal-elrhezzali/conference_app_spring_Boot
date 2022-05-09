38271c7ab2830a250ddea166f338efa37e43ca9d
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-05-09 15:12:39 +0200

    handling different environments: adding a production specific properties file

--- src/main/java/com/manal/conferencedemospringboot/repositories/SessionRepository.java
+++ src/main/java/com/manal/conferencedemospringboot/repositories/SessionRepository.java
@@ -3,7 +3,7 @@ package com.manal.conferencedemospringboot.repositories;
 import com.manal.conferencedemospringboot.models.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//by extending repository we now have find, update, delete and a bunch
+//by extending repository we now have the operations: find, update, delete and a bunch
 // of other operations already set up and usable
 // for us on our Session JPA class
 public interface SessionRepository extends JpaRepository<Session, Long> {
