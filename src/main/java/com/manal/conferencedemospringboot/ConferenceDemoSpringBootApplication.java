c496addc48412ea364effbdc189959a7d815dba0
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-05-12 01:47:57 +0200

    1st way to create a basic standalone JAR file to run ur app: utilize maven to create JAR files that you can use to run with the Java command

--- src/main/java/com/manal/conferencedemospringboot/ConferenceDemoSpringBootApplication.java
+++ src/main/java/com/manal/conferencedemospringboot/ConferenceDemoSpringBootApplication.java
@@ -2,6 +2,7 @@ package com.manal.conferencedemospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class ConferenceDemoSpringBootApplication {
--- src/main/java/com/manal/conferencedemospringboot/config/PersistenceConfiguration.java
+++ src/main/java/com/manal/conferencedemospringboot/config/PersistenceConfiguration.java
@@ -7,15 +7,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PersistenceConfiguration {
-  //we'll override the Dta source bean provided by te data jpa starter
+  //we'll override the Data source bean provided by te data jpa starter
   //when this method returns the DataSource object Spring looks for it
   // and tries see if one already exists in the Spring context
-  //  if it does, it will replace its definition with the one that it found
+//    if it does, it will replace its definition with the one that it found
   @Bean
   public DataSource dataSource() {
   //the body of the method is where the configuration actually happens
     DataSourceBuilder builder = DataSourceBuilder.create();
     builder.url("jdbc:postgresql://localhost:5432/conference_app");
+    //added this to know to see if the bean definition gets created
     System.out.println("**********My custom datasource bean has been initialized and set**********");
     return builder.build();
   }
