8e4ba82cd51d41e7e72d9e1520b2b262382b57bf
El-rhezzali Manal <manal.gzl99@gmail.com>
2022-05-12 00:01:06 +0200

    Overriding spring boot with Java config

---
+++ src/main/java/com/manal/conferencedemospringboot/config/PersistenceConfiguration.java
@@ -0,0 +1,23 @@
+package com.manal.conferencedemospringboot.config;
+
+import javax.sql.DataSource;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PersistenceConfiguration {
+  //we'll override the Dta source bean provided by te data jpa starter
+  //when this method returns the DataSource object Spring looks for it
+  // and tries see if one already exists in the Spring context
+  //  if it does, it will replace its definition with the one that it found
+  @Bean
+  public DataSource dataSource() {
+  //the body of the method is where the configuration actually happens
+    DataSourceBuilder builder = DataSourceBuilder.create();
+    builder.url("jdbc:postgresql://localhost:5432/conference_app");
+    System.out.println("**********My custom datasource bean has been initialized and set**********");
+    return builder.build();
+  }
+
+}
